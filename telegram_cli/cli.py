b7aded7784b0a2640a4e0a3d1119640b06bf7a98
Maurizio Branca <maurizio.branca@gmail.com>
2023-02-11 08:38:43 +0100

    Add `parse-mode` and `debug` options
    Refs: #zmoog/til#15

--- telegram_cli/cli.py
+++ telegram_cli/cli.py
@@ -5,9 +5,12 @@ from . import telegram
 
 @click.group()
 @click.version_option()
-def cli():
+@click.option('--debug', is_flag=True, help="Enable debug mode")
+@click.pass_context
+def cli(ctx: click.Context, debug: bool):
     "Python CLI tool and library for sending messages to Telegram"
-
+    ctx.ensure_object(dict)
+    ctx.obj['debug'] = debug
 
 @cli.group()
 @click.pass_context
@@ -24,11 +27,19 @@ def message(ctx: click.Context):
     "--chat-id",
     required=True,
 )
+@click.option(
+    '--parse-mode',
+    type=click.Choice(
+        ['HTML', 'MarkdownV2'],
+        case_sensitive=False
+    ),
+    default=None
+)
 @click.pass_context
-def send(ctx: click.Context, text: str, chat_id: str):
+def send(ctx: click.Context, text: str, chat_id: str, parse_mode: str):
     
-    client = telegram.Client.from_envorinment()
-    resp = client.send(text, chat_id)
+    client = telegram.Client.from_envorinment(debug=ctx.obj["debug"])
+    resp = client.send(text, chat_id, parse_mode=parse_mode)
 
     message_id = resp.get("result", {}).get("message_id", "No message id found")
     click.echo(f"message-id: {message_id}")
--- telegram_cli/telegram.py
+++ telegram_cli/telegram.py
@@ -7,11 +7,12 @@ import requests
 class Client:
     """Telegram client."""
 
-    def __init__(self, token: str):
+    def __init__(self, token: str, debug: bool = False):
         self.token = token
+        self.debug = debug
 
     @staticmethod
-    def from_envorinment() -> "Client":
+    def from_envorinment(debug: bool = False) -> "Client":
         """Create a client from the TELEGRAM_TOKEN environment variable."""
 
         if "TELEGRAM_TOKEN" not in os.environ:
@@ -20,20 +21,28 @@ class Client:
                 "Please set it to your Telegram API token."
             )
 
-        return Client(token=os.environ['TELEGRAM_TOKEN'])
+        return Client(token=os.environ['TELEGRAM_TOKEN'], debug=debug)
 
-    def send(self, msg: str, chat_id: str) -> Dict[str, Any]:
+    def send(self, msg: str, chat_id: str, parse_mode: str = None) -> Dict[str, Any]:
         """Send a message to a chat."""
 
         params = {
             'chat_id': chat_id,
-            'text': msg
+            'text': msg,
+            'parse_mode': parse_mode,
         }
+
+        if self.debug:
+            print(f"params: {params}")
+            print(f"Sending message to chat {chat_id}: {msg}")
 
         r = requests.get(
             f'https://api.telegram.org/{self.token}/sendMessage',
             params=params
         )
+
+        if self.debug:
+            print(r.text)
 
         r.raise_for_status()
         
